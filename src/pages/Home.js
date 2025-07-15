e0ba3dbc06c07d5b850ca4f0c3fc628e2b8ae49c
zues13bhai01 <gunjansiwach767@gmail.com>
2025-07-16 01:15:29 +0530

    Updated Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -108,7 +108,7 @@ export default function Home() {
                 alt={heroSlides[currentSlide].title}
                 className="w-full h-full object-cover object-center"
               />
-              <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/60 to-black/30" />
+              <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/60 to-black/30" >
             </div>
 
             {/* Content */}
